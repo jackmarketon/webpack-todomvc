dec92b1804ff946cd85dd3f9aa7d58fdc31cb093
Igor Hlina <srigi@srigi.sk>
2015-03-09 14:21:15 +0100

    Initiall commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+/* jshint node: true */
+'use strict';
+
+var path = require('path');
+
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+   index: './index.jsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+
+  module: {
+    loaders: [
+      { test: /\.css$/,
+        loaders: ['style', 'css']},
+
+      { test: /\.less$/,
+        loaders: ['style', 'css', 'less']},
+
+      { test: /\.(jsx|js)$/,
+        include: /src/,
+        loader: 'babel?experimental&optional=runtime'},
+
+      // static assets files
+      { test: /\.gif$/,
+        loader: 'url?limit=10240&mimetype=image/gif'},
+
+      { test: /\.jpg$/,
+        loader: 'url?limit=10240&mimetype=image/jpg'},
+
+      { test: /\.png$/,
+        loader: 'url?limit=10240&mimetype=image/png'},
+
+      { test: /\.svg$/,
+        loader: 'url?limit=20480&mimetype=image/svg+xml'},
+
+      { test: /\.(woff2|woff|ttf|eot)$/,
+        loader: 'url'}
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+
+  plugins: [
+  ]
+};
