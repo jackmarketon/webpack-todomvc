81d6695989a61d5b81409e087a47fd4cb7bb71fe
Igor Hlina <srigi@srigi.sk>
2015-03-11 09:17:40 +0100

    Splitting webpack.config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,10 @@
 /* jshint node: true */
 'use strict';
 
+var webpackLoadersConfig = require('./webpack/loaders.config');
+var webpackPluginsConfig = require('./webpack/plugins.config');
+var webpackResolveConfig = require('./webpack/resolve.config');
+
 
 module.exports = function(config) {
   config.set({
@@ -27,34 +31,10 @@ module.exports = function(config) {
     webpack: {
       devtool: 'inline-source-map',
       module: {
-        loaders: [
-          { test: /\.less$/,
-            loaders: ['style', 'css?sourceMap', 'less']},
-
-          { test: /\.(jsx|js)$/,
-            include: [/src/, /test/],
-            loader: 'babel?experimental'},
-
-          // static assets files
-          { test: /\.gif$/,
-            loader: 'url?limit=10240&mimetype=image/gif'},
-
-          { test: /\.jpg$/,
-            loader: 'url?limit=10240&mimetype=image/jpg'},
-
-          { test: /\.png$/,
-            loader: 'url?limit=10240&mimetype=image/png'},
-
-          { test: /\.svg$/,
-            loader: 'url?limit=20480&mimetype=image/svg+xml'},
-
-          { test: /\.(woff2|woff|ttf|eot)$/,
-            loader: 'url'}
-        ]
+        loaders: webpackLoadersConfig
       },
-      resolve: {
-        extensions: ['', '.js', '.jsx', '.json']
-      }
+      plugins: webpackPluginsConfig,
+      resolve: webpackResolveConfig
     },
 
     webpackServer: {
