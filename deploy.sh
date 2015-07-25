57ebd5daf4d022af5f7291816aedc7bea12721bf
Jackson Marketon <jmarketon@gmail.com>
2015-07-24 19:35:50 -0500

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+
+npm run build
+git config credential.helper 'store --file=.git/credentials'
+git config --global push.default simple
+echo 'https://${GH_TOKEN}:@github.com' > .git/credentials
+git add -A .
+git status
+git commit -m 'Travis CI Build'
+git remote -v
+git remote set-url origin https://github.com/${TRAVIS_REPO_SLUG}.git
+git push
