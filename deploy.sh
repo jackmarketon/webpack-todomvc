c56b2ead00c814079e30d2ad6e0174aa68359184
Jackson Marketon <jmarketon@gmail.com>
2015-07-24 19:56:58 -0500

    updates

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,15 @@ git config --global push.default simple
 git config user.email "${USER_EMAIL}"
 git config user.name "${USER_NAME}"
 echo 'https://${GH_TOKEN}:@github.com' > .git/credentials
-npm run build
-git add -A .
-git status
-git commit -m 'Travis CI Build [ci skip]'
-git remote -v
-git remote set-url origin https://github.com/${TRAVIS_REPO_SLUG}.git
-git push
+git checkout ${TRAVIS_BRANCH}
+branch = $(git symbolic-ref --short -q HEAD)
+if branch != 'master'
+then
+	npm run build
+	git add -A .
+	git status
+	git commit -m 'Travis CI Build [ci skip]'
+	git remote -v
+	git remote set-url origin https://github.com/${TRAVIS_REPO_SLUG}.git
+	git push
+fi
