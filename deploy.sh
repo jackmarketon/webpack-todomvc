aebdc59a03e6dee6e58b2b0c6a905422ea1d5666
Jackson Marketon <jack@loki.local>
2015-07-24 20:25:03 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ git config credential.helper 'store --file=.git/credentials'
 git config --global push.default simple
 git config user.email "${USER_EMAIL}"
 git config user.name "${USER_NAME}"
-echo 'https://${GH_TOKEN}:@github.com' > .git/credentials
 git checkout ${TRAVIS_BRANCH}
 branch=$(git symbolic-ref --short -q HEAD)
 if ! [ "$branch" == "master" ]; then
@@ -14,6 +13,6 @@ if ! [ "$branch" == "master" ]; then
 	git status
 	git commit -m "Travis CI Build [ci skip]"
 	git remote -v
-	git remote set-url origin https://github.com/${TRAVIS_REPO_SLUG}.git
+	git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 	git push
 fi
