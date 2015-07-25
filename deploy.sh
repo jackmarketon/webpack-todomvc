d189c0fa09caae5095eac752a8d878bc46d28b9f
Jackson Marketon <jmarketon@gmail.com>
2015-07-24 19:50:55 -0500

    build update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ set -ev
 
 git config credential.helper 'store --file=.git/credentials'
 git config --global push.default simple
+git config user.email "${USER_EMAIL}"
+git config user.name "${USER_NAME}"
 echo 'https://${GH_TOKEN}:@github.com' > .git/credentials
 npm run build
 git add -A .
