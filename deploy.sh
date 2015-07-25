1d0ffac6c9041c98d8169cb8f5bb6c5739760bed
Jackson Marketon <jack@loki.local>
2015-07-24 20:59:33 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git config --global push.default simple
 git config user.email "${USER_EMAIL}"
 git config user.name "${USER_NAME}"
 git checkout ${TRAVIS_BRANCH}
-if ! [ "${TRAVIS_BRANCH}" == "master" ]; then
+# if ! [ "${TRAVIS_BRANCH}" == "master" ]; then
 	rm ./stats.json 2> /dev/null
 	npm run build
 	git add -A .
@@ -15,4 +15,4 @@ if ! [ "${TRAVIS_BRANCH}" == "master" ]; then
 	git remote -v
 	git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 	git push
-fi
+# fi
