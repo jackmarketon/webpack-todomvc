c3a6f1fceb2829d124e509196c99134a066e7ffc
Jackson Marketon <jack@loki.local>
2015-07-24 20:47:13 -0500

    updates?

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git config --global push.default simple
 git config user.email "${USER_EMAIL}"
 git config user.name "${USER_NAME}"
 git checkout ${TRAVIS_BRANCH}
-if ! [ "$(git symbolic-ref --short -q HEAD)" == "master" ]; then
+if ! [ "${TRAVIS_BRANCH}" == "master" ]; then
 	rm ./stats.json 2> /dev/null
 	npm run build
 	git add -A .
